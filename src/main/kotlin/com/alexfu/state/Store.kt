e1743861700d922354550cd6e0be628773a3ba87
Alex Fu <alex.fu914@gmail.com>
2023-02-25 16:58:23 -0500

    Remove unnecessary processor.

--- src/main/kotlin/com/alexfu/state/Store.kt
+++ src/main/kotlin/com/alexfu/state/Store.kt
@@ -2,29 +2,18 @@ package com.alexfu.state
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 typealias Action<T> = (T) -> T
 
 open class Store<T : Any>(initialState: T) {
     private val stateFlow = MutableStateFlow(initialState)
-    private val stateProcessor = Channel<Action<T>>()
     private val scope = CoroutineScope(Dispatchers.Default)
 
     val state: T
         get() = stateFlow.value
-
-    init {
-        scope.launch {
-            stateProcessor.receiveAsFlow()
-                .map { action -> action(state) }
-                .collect { newState ->
-                    stateFlow.value = newState
-                }
-        }
-    }
 
     fun observeState(): Flow<T> {
         return stateFlow
@@ -32,7 +21,7 @@ open class Store<T : Any>(initialState: T) {
 
     fun updateState(action: Action<T>) {
         scope.launch {
-            stateProcessor.send(action)
+            stateFlow.value = action(stateFlow.value)
         }
     }
 }
